b6e66b15868c7b75c2c5c9412063325157f94105
awsandy <awsandy@amazon.co.uk>
2020-07-06 21:11:07 +0100

    sync

--- test/variables.tf
+++ test/variables.tf
@@ -1,17 +1,17 @@
 variable "mycount" {
-    default = 1
+    default = 2
 }
 
 variable "aws_vpc" {
     type    = list
-    default = ["vpc-10-1", "vpc-10-2", "vpc-devt-proja", "vpc-devt-projx"]
+    default = ["vpc-10-1", "vpc-10-2"]
 }
 
 variable "aws_cidr" {
   default = {
     "vpc-10-1" = "10.1.0.0/16"
-    "vpc-10-2" = "10.2.0.0/16"
     "vpc-devt-proja" = "10.3.0.0/16"
+    "vpc-10-2" = "10.2.0.0/16"
     "vpc-devt-projx" = "10.4.0.0/16"
   }
 }
