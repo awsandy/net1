ea74983d63af570a749fcc74bd0873fc67566776
awsandy <awsandy@amazon.co.uk>
2020-07-11 21:28:46 +0100

    rt

--- challenge-1/def-rtb.tf
+++ challenge-1/def-rtb.tf
@@ -1,9 +1,3 @@
 data "aws_route_table" "defrt" {
-  subnet_id = data.aws_subnet.c9subnet.id
-  vpc_id = data.aws_vpc.dvpc.id
-  filter {
-    name = "association.main"
-    values = [true]
-  }
   route_table_id  = "rtb-9bb8b2f2"
 }
