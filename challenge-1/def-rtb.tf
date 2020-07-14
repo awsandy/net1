cc44f28430ba1e130818c8c419cb7d0200525062
awsandy <awsandy@amazon.co.uk>
2020-07-14 11:21:58 +0100

    defrt

--- challenge-1/def-rtb.tf
+++ challenge-1/def-rtb.tf
@@ -1,8 +1,8 @@
-data "aws_route_table" "rt-10-1" { 
+data "aws_route_table" "defrt" { 
   #subnet_id=data.aws_subnet.c9subnet.id
   #filter {
   #  values = [ data.aws_subnet.c9subnet.id ]
   ##  name = "association.subnet-id"
   #}
-  route_table_id=var.rtbid-10-1
+  route_table_id=var.rtbid
 }
