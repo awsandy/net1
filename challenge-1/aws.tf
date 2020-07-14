06b4b7ac87de3fadb1656949c00561292e22450f
awsandy <awsandy@amazon.co.uk>
2020-07-14 08:01:27 +0100

    aws.tf

--- challenge-1/aws.tf
+++ challenge-1/aws.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                  = "eu-west-3"
+  region                  = "eu-west-1"
   shared_credentials_file = "~/.aws/credentials"
   version                 = "= 2.58"
   profile                 = "default"
