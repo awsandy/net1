3bb04bcbadba59050626e0afbc769ff97ffef42e
awsandy <awsandy@amazon.co.uk>
2020-07-14 21:20:29 +0100

    up

--- challenge-1/rt-entries.tf
+++ challenge-1/rt-entries.tf
@@ -1,17 +1,6 @@
 resource "aws_route" "route-10-0" {
   route_table_id            = data.aws_route_table.defrt.id
-  destination_cidr_block    = "10.0.0.0/16"
+  destination_cidr_block    = "10.0.0.0/8"
   transit_gateway_id = data.aws_ec2_transit_gateway.mytgw.id
 }
 
-resource "aws_route" "route-10-1" {
-  route_table_id            = data.aws_route_table.defrt.id
-  destination_cidr_block    = "10.1.0.0/16"
-  transit_gateway_id = data.aws_ec2_transit_gateway.mytgw.id
-}
-
-resource "aws_route" "route-10-2" {
-  route_table_id            = data.aws_route_table.defrt.id
-  destination_cidr_block    = "10.2.0.0/16"
-  transit_gateway_id = data.aws_ec2_transit_gateway.mytgw.id
-}
