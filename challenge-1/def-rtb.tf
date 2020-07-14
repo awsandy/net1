4bb0e7de61131e54cf2a06c353cce60beaf1bc2b
awsandy <awsandy@amazon.co.uk>
2020-07-14 08:49:00 +0100

    test

--- challenge-1/def-rtb.tf
+++ challenge-1/def-rtb.tf
@@ -1,8 +1,8 @@
 data "aws_route_table" "defrt" { 
   #subnet_id="subnet-e09fe49b"
-  #filter {
-  #  name = "association.subnet-id"
-  #  values = [data.aws_instance.c9.subnet_id]
-  #}
-  route_table_id=var.rtbid
+  filter {
+    name = "association.subnet-id"
+    values = [data.aws_subnet.c9subnet.subnet_id]
+  }
+  #route_table_id=var.rtbid
 }
