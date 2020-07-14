4fc69a9e79fa86a88242f137d4380316b406a093
awsandy <awsandy@amazon.co.uk>
2020-07-14 09:11:01 +0100

    ls3

--- challenge-1/labsol.sh
+++ challenge-1/labsol.sh
@@ -3,3 +3,9 @@ vpcid=`aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query "Vpc
 rtbid=`aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcid" "Name=association.main,Values=true" --query "RouteTables[0].RouteTableId" | tr -d '"'`
 export TF_VAR_rtbid=$rtbid
 echo $TF_VAR_rtbid
+
+tsubid==`aws ec2 describe-subnets --filters "Name=cidr-block,Values=10.1.4.0/24" --query "Subnets[].SubnetId" | jq .[] | tr -d '"'` 
+subid=`echo $tsubid | tr -d '='`
+echo $subid
+rtbid2=`aws ec2 describe-route-tables --filters "Name=association.subnet-id,Values=$subid" --query "RouteTables[0].RouteTableId" | tr -d '"'`
+echo $rtbid2
