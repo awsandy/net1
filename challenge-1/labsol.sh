68e0eb60bf7585561fb4fe49fb939ca582a6e3c4
awsandy <awsandy@amazon.co.uk>
2020-07-14 08:20:31 +0100

    ls2

--- challenge-1/labsol.sh
+++ challenge-1/labsol.sh
@@ -2,3 +2,4 @@
 vpcid=`aws ec2 describe-vpcs --filters "Name=isDefault,Values=true" --query "Vpcs[].VpcId" | jq .[0] | tr -d '"'` 
 rtbid=`aws ec2 describe-route-tables --filters "Name=vpc-id,Values=$vpcid" "Name=association.main,Values=true" --query "RouteTables[0].RouteTableId" | tr -d '"'`
 echo $rtbid
+#
