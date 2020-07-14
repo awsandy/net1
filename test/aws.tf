00fb3749238c66ec2d5006696db0bf601e1aabdd
awsandy <awsandy@amazon.co.uk>
2020-07-14 14:16:36 +0100

    init

--- test/aws.tf
+++
@@ -1,6 +0,0 @@
-provider "aws" {
-  region                  = "eu-west-3"
-  shared_credentials_file = "~/.aws/credentials"
-  version                 = "= 2.58"
-  profile                 = "default"
-}
--- test/data_mytgw.tf
+++
@@ -1,6 +0,0 @@
-data "aws_ec2_transit_gateway" "mytgw" {
-  filter {
-    name   = "options.amazon-side-asn"
-    values = ["64512"]
-  }
-}
